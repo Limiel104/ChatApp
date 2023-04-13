5a2dac02f01ee61f36b3e8d4cdfa11654de6ef94
Limiel104 <a.myjak1995@gmail.com>
2023-04-13 18:58:46 +0200

    Fixed instrumented tests by expanding di with missing functions

--- app/src/androidTest/java/com/example/chatapp/di/TestAppModule.kt
+++ app/src/androidTest/java/com/example/chatapp/di/TestAppModule.kt
@@ -1,8 +1,15 @@
 package com.example.chatapp.di
 
 import com.example.chatapp.data.repository.AuthRepositoryImpl
+import com.example.chatapp.data.repository.UserStorageRepositoryImpl
 import com.example.chatapp.domain.repository.AuthRepository
+import com.example.chatapp.domain.repository.UserStorageRepository
+import com.example.chatapp.domain.use_case.*
+import com.example.chatapp.util.Constants
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -22,5 +29,29 @@ object TestAppModule {
     @Singleton
     fun provideAuthRepository(firebaseAuth: FirebaseAuth): AuthRepository {
         return AuthRepositoryImpl(firebaseAuth)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserRef(): CollectionReference {
+        return Firebase.firestore.collection(Constants.USERS_COLLECTION)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserStorageRepository(userRef: CollectionReference): UserStorageRepository {
+        return UserStorageRepositoryImpl(userRef)
+    }
+
+    @Provides
+    @Singleton
+    fun provideChatUseCases(): ChatUseCases {
+        return ChatUseCases(
+            validateEmailUseCase = ValidateEmailUseCase(),
+            validateLoginPasswordUseCase = ValidateLoginPasswordUseCase(),
+            validateSignupPasswordUseCase = ValidateSignupPasswordUseCase(),
+            validateConfirmPasswordUseCase = ValidateConfirmPasswordUseCase(),
+            validateNameUseCase = ValidateNameUseCase()
+        )
     }
 }
