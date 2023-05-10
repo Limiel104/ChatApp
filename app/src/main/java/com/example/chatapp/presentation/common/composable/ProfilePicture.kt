2a833bff5d79279840b110247b76156a1892407b
Limiel104 <a.myjak1995@gmail.com>
2023-05-10 18:06:07 +0200

    Changed the shape of the profile picture click ripple effect to circle.

--- app/src/main/java/com/example/chatapp/presentation/common/composable/ProfilePicture.kt
+++ app/src/main/java/com/example/chatapp/presentation/common/composable/ProfilePicture.kt
@@ -2,17 +2,21 @@ package com.example.chatapp.presentation.common.composable
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.chatapp.R
+import com.example.chatapp.ui.theme.ChatAppTheme
 import com.example.chatapp.util.Constants
 
 @Composable
@@ -26,8 +30,9 @@ fun ProfilePicture(
         Card(
             modifier = Modifier
                 .size(size.dp)
+                .clip(shape = CircleShape)
                 .clickable { onClick() },
-            shape = RoundedCornerShape(size.dp),
+            shape = CircleShape,
             backgroundColor = Color.LightGray
         ) {
             AsyncImage(
@@ -62,5 +67,18 @@ fun ProfilePicture(
                 error = painterResource(R.drawable.ic_person)
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun ProfilePicturePreview() {
+    ChatAppTheme() {
+        ProfilePicture(
+            size = 40,
+            profilePictureUrl = "url",
+            isClickable = true,
+            onClick = {}
+        )
     }
 }
